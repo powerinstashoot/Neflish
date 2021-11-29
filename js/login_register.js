ebe799d1eec7cc0ddf34f292b554028ed5eb0f3b
jauregi13 <jon.j1305@gmail.com>
2021-11-29 18:16:25 +0100

    Login

---
+++ js/login_register.js
@@ -0,0 +1,16 @@
+function register_erakutsi() {
+	
+	document.getElementById('login').style.display="none";
+	document.getElementById('register').style.display="block";
+}
+
+function login_erakutsi() {
+	
+	document.getElementById('login').style.display="block";
+	document.getElementById('register').style.display="none";
+}
+
+function pasahitzaEgiaztatu() {
+	
+	var pasahitza = document.getElementById('pasahitza').value;
+}
