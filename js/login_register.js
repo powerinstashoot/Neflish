ecc64c9b2baae7c378225b6d962f3c00af3382e9
Azkunaga <aritzazkunaga@users.noreply.github.com>
2021-11-30 00:53:20 +0100

    Login eta singup

--- js/login_register.js
+++ js/login_register.js
@@ -1,16 +1,41 @@
-function register_erakutsi() {
-	
-	document.getElementById('login').style.display="none";
-	document.getElementById('register').style.display="block";
+
+function aldatuActiveLog(){
+	document.getElementById("divLog").classList.add("is-active");
+	document.getElementById("divReg").classList.remove("is-active");
 }
 
-function login_erakutsi() {
-	
-	document.getElementById('login').style.display="block";
-	document.getElementById('register').style.display="none";
+function aldatuActiveReg(){
+	document.getElementById("divReg").classList.add("is-active");
+	document.getElementById("divLog").classList.remove("is-active");
+
 }
 
-function pasahitzaEgiaztatu() {
-	
-	var pasahitza = document.getElementById('pasahitza').value;
+function izenaEgiaztatu(){
+	var izena = document.getElementById("izen-abizenak");
+	var regex= /^[A-Z]([a-z]+){2,}(\ ([A-Z][a-z]+){1,})+$/;
+	if(izena.value.length!=0 && !regex.test(izena.value)){
+		izena.style.borderColor="#E50914";
+	}else{
+		izena.style.borderColor="";
+	}
+}
+
+function emailEgiaztatu(){
+	var email = document.getElementById("signup-email");
+	var regex= /^[a-zA-Z0-9]{1,}\@[a-z]{2,}\.[a-z]{2,}$/;
+	if(email.value.length!=0 && !regex.test(email.value)){
+		email.style.borderColor="#E50914";
+	}else{
+		email.style.borderColor="";
+	}
+}
+
+function pasahitzaEgiaztatu(){
+	var pas1 = document.getElementById("signup-password").value;
+	var pas2 = document.getElementById("signup-password-confirm");
+	if(pas2.value.length!=0 && pas1!=pas2.value){
+		pas2.style.borderColor="#E50914";
+	}else{
+		pas2.style.borderColor="";
+	}
 }
--- php/login.php
+++ php/login.php
@@ -1,8 +1,27 @@
 <?php
-
-
-
-
-
-
-?>
+	if(isset($_POST['login-email'])){
+		$konexioa = new mysqli ("localhost", "root", "", "sza") or die ("Ezin izan da DB-ra konektatu");
+		$erabemail = $_POST['login-email'];
+		$pasahitza = $_POST['login-password'];
+		$eskaera = "SELECT * FROM Erabiltzailea WHERE email = '$erabemail'";
+		$emaitza = $konexioa->query($eskaera);
+		if(!$emaitza){
+			die("Errorea querya gauzatzerakoan. ").$emaitza->error;
+		}else{
+			$errenkadak = $emaitza->num_rows;
+			$konexioa->close();
+			if($errenkadak==1){
+				$row=$emaitza->fetch_object();
+				if(hash_equals($row->pasahitza, crypt($pasahitza, $row->pasahitza))){
+					include 'IncreaseGlobalCounter.php';
+					$_SESSION['email'] = $erabemail;
+					echo ("<script> alert('Saioa hasi duzu! Ongi etorri'); window.location='neflish.html'</script>");
+				}else{
+					echo("<script> alert('Errorea kautotzerakoan')</script>");
+				}
+			}else{
+				echo("<script> alert('Errorea kautotzerakoan')</script>");
+			}
+		}
+	}
+	?>
