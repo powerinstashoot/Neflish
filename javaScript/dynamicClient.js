4de4d27104df00f756f1967675c048ab33b458e5
Azkunaga <aritzazkunaga@users.noreply.github.com>
2021-11-29 15:24:27 +0100

    berrantolaketa

---
+++ javaScript/dynamicClient.js
@@ -0,0 +1,41 @@
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function onClickMenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+//STICKY NAVBAR
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyNavbar()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    alert("asd");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
