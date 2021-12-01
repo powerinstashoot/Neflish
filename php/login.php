fd99f72488df172493412d8009c47202b1230561
Azkunaga <aritzazkunaga@users.noreply.github.com>
2021-12-01 11:18:17 +0100

    Login eta singup aldaketak

--- php/login.php
+++ php/login.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Neflish</title>
-	<meta charset="ISO-8859-1">
-	<link rel="stylesheet" type="text/css" href="../css/styles.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-	<link rel="stylesheet" type="text/css" href="../css/login.css">
-	<link rel="icon" href="../img/NeflishLogo3.png">
-	<script type="text/javascript" src="../js/login_register.js"></script>
-</head>
-<body>
-	<section class="forms-section">
-  <!--<h1 class="section-title">LOGEATU/ERREGISTRATU</h1>-->
-  <div class="forms">
-    <div class="form-wrapper is-active" id="divLog">
-      <button type="button" class="switcher switcher-login" onclick="aldatuActiveLog()">
-        Login
-        <span class="underline"></span>
-      </button>
-      <form class="form form-login" method="post" action="login.php">
-        <fieldset>
-          <legend>Mesedez, erabiltzailea eta pasahitza sar ezazu saioa hasteko.</legend>
-          <div class="input-block">
-            <label for="login-email">E-mail</label>
-            <input id="login-email" name="login-email" type="email" required>
-          </div>
-          <div class="input-block">
-            <label for="login-password">Password</label>
-            <input id="login-password" name="login-password" type="password" required>
-            <span id="ErabErr" style="display: none;"></span>
-          </div>
-        </fieldset>
-        <button type="submit" class="btn-login">Logeatu</button>
-      </form>
-    </div>
-    <?php
+<?php
 		if(isset($_POST['login-email'])){
 			$konexioa = new mysqli ("localhost", "root", "", "sza") or die ("Ezin izan da DB-ra konektatu");
 			$erabemail = $_POST['login-email'];
@@ -63,38 +27,3 @@
 			}
 		}
 	?>
-    <div class="form-wrapper" id="divReg">
-      <button type="button" class="switcher switcher-signup" onclick="aldatuActiveReg()">
-        Sign Up
-        <span class="underline"></span>
-      </button>
-      <form class="form form-signup" method="post" action="signup.php">
-        <fieldset>
-          <legend>Mesedez, izen abizenak, erabiltzailea, emaila eta pasahitza bitan erregistratzeko.</legend>
-          <div class="input-block">
-            <label for="izen-abizenak">Izen Abizenak*</label>
-            <input id="izen-abizenak" name="izen-abizenak" onblur="izenaEgiaztatu()" required>
-            <span id="IzenErr">Izen Abizenak ez du formatu egokia</span>
-          </div>
-          <div class="input-block">
-            <label for="signup-email">E-mail*</label>
-            <input id="signup-email" name="signup-email" type="email" onblur="emailEgiaztatu()" pattern="/^[a-zA-Z0-9]{1,}\@[a-z]{2,}\.[a-z]{2,}$/" required>
-            <span id="EmailErr" class="err">Emaila ez du formatu egokia</span>
-          </div>
-          <div class="input-block">
-            <label for="signup-password">Pasahitza*</label>
-            <input id="signup-password" name="signup-password" type="password" required>
-          </div>
-          <div class="input-block">
-            <label for="signup-password-confirm">Pasahitza errepikatu*</label>
-            <input id="signup-password-confirm" name="signup-password-confirm" type="password" onblur="pasahitzaEgiaztatu()"required>
-            <span id="PasErr">Pasahitzak ez dira berdinkak</span>
-          </div>
-        </fieldset>
-        <button type="submit" id="btn-signup" class="btn-signup">Erregistratu</button>
-      </form>
-    </div>
-  </div>
-</section>
-</body>
-</html>
--- php/signup.php
+++ php/signup.php
@@ -1,28 +1,31 @@
 <?php
     $hutsa = '/^\s*$/';
-    if(isset($_POST['izen-abizenak']) && !preg_match($hutsa, $_POST['izen-abizenak']) && !preg_match($hutsa, $_POST['signup-email']) && !preg_match($hutsa, $_POST['signup-password']) && !preg_match($hutsa, $_POST['signup-password-confirm'])){
+    if (isset($_POST['izen-abizenak'])) {
+      if(!preg_match($hutsa, $_POST['izen-abizenak']) && !preg_match($hutsa, $_POST['signup-email']) && !preg_match($hutsa, $_POST['signup-password']) && !preg_match($hutsa, $_POST['signup-password-confirm'])){
 
-      $emaila = $_POST['signup-email'];
-      $pas1=$_POST['signup-password'];
-      $pas2=$_POST['signup-password-confirm'];
-      $izena=$_POST['izen-abizenak'];
-      $izenPattern= '/^[A-Z]([a-z]+){2,}(\ ([A-Z][a-z]+){1,})+$/';
-      $emailPattern='/^[a-zA-Z0-9]{1,}\@[a-z]{2,}\.[a-z]{2,}$/';
+        $emaila = $_POST['signup-email'];
+        $pas1=$_POST['signup-password'];
+        $pas2=$_POST['signup-password-confirm'];
+        $izena=$_POST['izen-abizenak'];
+        $izenPattern= '/^[A-Z]([a-z]+){2,}(\ ([A-Z][a-z]+){1,})+$/';
+        $emailPattern='/^[a-zA-Z0-9]{1,}\@[a-z]{2,}\.[a-z]{2,}$/';
 
-      if(preg_match($izenPattern, $_POST['izen-abizenak']) && preg_match($emailPattern, $emaila) && $pas1==$pas2) {
-          	$esteka = mysqli_connect ("localhost", "root", "", "sza") or die ("Errorea Dbra konektatzerakoan");
-            $pasahitza= crypt($_POST['signup-password']);
-          		$sql="INSERT INTO erabiltzailea(izena,email,pasahitza) VALUES('$izena', '$emaila', '$pasahitza')";
-        			$ema = mysqli_query($esteka, $sql);  
-        			if (!$ema) {
-        				die('Errorea query-a gauzatzerakoan: '.mysqli_error($esteka));
-        			}
-        			mysqli_close($esteka);
-              echo "<script> alert('Ondo erregistratu zara'); window.location='login.php'</script>";
+        if(preg_match($izenPattern, $_POST['izen-abizenak']) && preg_match($emailPattern, $emaila) && $pas1==$pas2) {
+            	$esteka = mysqli_connect ("localhost", "root", "", "sza") or die ("Errorea Dbra konektatzerakoan");
+              @$pasahitza= crypt($_POST['signup-password']);
+            		$sql="INSERT INTO erabiltzailea(izena,email,pasahitza) VALUES('$izena', '$emaila', '$pasahitza')";
+          			$ema = mysqli_query($esteka, $sql);  
+          			if (!$ema) {
+          				echo "<script> alert('Erabiltzailea jada existitzen da');</script>";
+                  exit();
+          			}
+          			mysqli_close($esteka);
+                echo "<script> alert('Ondo erregistratu zara');</script>";
+        }else{
+          echo "<script> alert('Errorea erregistratzerakoan');</script>";
+        }
       }else{
-        echo "<script> alert('Errorea erregistratzerakoan'); window.location='login.php'</script>";
+        echo "<script> alert('Errorea erregistratzerakoan');</script>";
       }
-    }else{
-      echo "<script> alert('Errorea erregistratzerakoan'); window.location='login.php'</script>";
     }
 ?>
