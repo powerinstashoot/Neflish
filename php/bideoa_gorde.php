541e786ceeafccf879d2cfdd645a87879336e129
saragracia8 <saragraciaurzelai@gmail.com>
2021-12-08 00:00:29 +0100

    bideoak gorde estiloa

--- php/bideoa_gorde.php
+++ php/bideoa_gorde.php
@@ -4,6 +4,7 @@
 	<head>
 		<meta charset="ISO-8859-1">
 		<link rel="stylesheet" type="text/css" href="../css/styles.css">
+		<link rel="stylesheet" type="text/css" href="../css/bideoGorde.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
 		<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 		<script type="text/javascript" src="../js/dynamicClient.js"></script>
@@ -15,37 +16,47 @@
 	<body class="bideoG">
 		<?php include 'menu.php' ?>
 		<div class="content">
-			<form action="../php/bideoa_gorde.php" method="POST" enctype="multipart/form-data">
-				<div class="inputgroup">
-					<label for="titulua">Titulua:</label>
-					<input type="text" id="titulua" name="titulua">
-				</div>
-				<div class="inputgroup">
-					<label for="linka">Linka:</label>
-					<input type="url" id="linka" name="linka">
-				</div>
-				<div class="inputgroup">
-					<label for="azalpena">Azalpena:</label>
-					<textarea rows="15" cols="70" id="azalpena" name="azalpena"></textarea>
-				</div>
-				<div class="inputgroup">
-					<label for="kategoria">Kategoria:</label>
-					<select id="kategoria" name="kategoria">
-						<option value="Komedia">Komedia</option>
-						<option value="Zientzia-fikzioa">Zientzia-fikzioa</option>
-						<option value="Beldurrezkoak">Beldurrezkoak</option>
-						<option value="Haurrentzako">Haurrentzako</option>
-					</select>
-				</div>
-				<div class="inputgroup">
-					<input type="file" id="img" name="img" accept="image/*" onchange="loadFile(event)"><br>
-					<img id="output" height="100" /><br>
-					<input type="button" id="hustu_img" value="EZABATU ARGAZKIA" onclick="ezabatuArgazkia()">
-				</div>
-				<div class="inputgroup">
-					<input type="submit" name="igo" onclick="return balidatu(this.form);">
-				</div>
-			</form>
+			<div class="container">
+				<form class="bideoaForm" action="../php/bideoa_gorde.php" method="POST" enctype="multipart/form-data">
+					<h2 class="izenburuaForm">Bideoa gehitu</h2>
+					<div class="inputgroup">
+						<label for="titulua">Titulua</label>
+						<input type="text" id="titulua" name="titulua">
+					</div>
+					<div class="inputgroup">
+						<label for="linka">Linka</label>
+						<input type="url" id="linka" name="linka">
+					</div>
+					<div class="inputgroup">
+						<label for="azalpena">Azalpena</label>
+						<textarea rows="15" cols="70" id="azalpena" name="azalpena"></textarea>
+					</div>
+					<div class="inputgroup">
+						<label for="kategoria">Kategoria</label>
+						<select id="kategoria" name="kategoria">
+							<option value="Komedia">Komedia</option>
+							<option value="Zientzia-fikzioa">Zientzia-fikzioa</option>
+							<option value="Beldurrezkoak">Beldurrezkoak</option>
+							<option value="Haurrentzako">Haurrentzako</option>
+						</select>
+					</div>
+					<div class="inputgroup">
+						<p class="plabel">Azaleko irudia</p>
+						<label for="img" class="custom-file-upload">
+    						<i class="fa fa-cloud-upload"></i> Irudia igo
+						</label>
+						<input type="file" id="img" name="img" accept="image/*" onchange="loadFile(event)">
+						<div class="imageContainer">
+							<img id="output" height="150" />
+						</div>
+						
+						<input type="button" id="hustu_img" value="Irudia ezabatu" onclick="ezabatuArgazkia()">
+					</div>
+					<div class="inputgroup mt2">
+						<input type="submit" class="btn-nagusia" name="igo" value="BIDALI" onclick="return balidatu(this.form);">
+					</div>
+				</form>
+			</div>
 			<?php
 			require_once('bideoak.php');
 
