7a4cf78930d666e15e520df6cc757273b451f8dd
jauregi13 <jon.j1305@gmail.com>
2021-12-09 23:03:47 +0100

    showImage.js komentatuta

--- js/showImage.js
+++ js/showImage.js
@@ -1,3 +1,7 @@
+/*
+Irudia igotzeko funtzioa. Irudia aukeratzerakoan, irudi hori agertuko da
+formularioan igotzeko prest.
+*/
 var loadFile = function(event) {
 	var image = document.getElementById('output');
 	image.src = URL.createObjectURL(event.target.files[0]);
@@ -12,6 +16,10 @@ var loadFile = function(event) {
 
 };
 
+/*
+Irudia aukeratzerakoan, irudi hori ezabatzeko funtzioa. Beste irudi bat
+aukeratu daiteke.
+*/
 function ezabatuArgazkia(){
 	document.getElementById("img").value='';
 	document.getElementById("output").style.visibility="hidden";
